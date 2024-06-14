999b5e47b136db294925e5acb13b51afed16c0c9
harddik25 <135815117+harddik25@users.noreply.github.com>
2024-06-14 20:06:32 +0200

    Create authMiddleware.js

---
+++ server/authMiddleware.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+exports.verifyToken = (req, res, next) => {
+  const token = req.cookies.token;
+  if (!token) {
+    return res.status(401).json({ message: 'No token, authorization denied' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    res.status(401).json({ message: 'Token is not valid' });
+  }
+};
+
+exports.isAdmin = async (req, res, next) => {
+  const user = await User.findById(req.user.id);
+  if (user.role !== 'admin') {
+    return res.status(403).json({ message: 'Access denied' });
+  }
+  next();
+};
