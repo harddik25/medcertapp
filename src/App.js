5e7498c2bdaa6bb23884a492e8567d41c45a56d5
harddik25 <135815117+harddik25@users.noreply.github.com>
2024-06-15 01:39:57 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Login from './components/Login';
 import UserProfile from './components/UserProfile';
 import Certificate from './components/Certificate';
@@ -31,7 +31,7 @@ function App() {
   }
 
   return (
-    <Router>
+    <>
       <WebSocketComponent />
       <Routes>
         <Route path="/login" element={<Login />} />
@@ -56,11 +56,10 @@ function App() {
           </ProtectedRoute>
         } />
       </Routes>
-    </Router>
+    </>
   );
 }
 
 export default App;
-
 
 
