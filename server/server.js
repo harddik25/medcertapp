e1cadc43f4333afa176092c1bd9f1be5c4723b93
harddik25 <135815117+harddik25@users.noreply.github.com>
2024-06-14 18:05:55 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,70 +1,27 @@
-const express = require('express');
+const app = require('./app');
 const http = require('http');
 const WebSocket = require('ws');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const cookieParser = require('cookie-parser');
-require('dotenv').config();
-const path = require('path');
+const PORT = process.env.PORT || 5000;
 
-// Импорт маршрутов
-const authRoutes = require('./routes/authRoutes');
-const certificateRoutes = require('./routes/certificateRoutes');
-const consultationRoutes = require('./routes/consultationRoutes');
-const adminRoutes = require('./routes/adminRoutes');
-const surveyRoutes = require('./routes/surveyRoutes');
-const appointmentRoutes = require('./routes/appointmentRoutes');
-
-const app = express();
 const server = http.createServer(app);
+
 const wss = new WebSocket.Server({ server });
 
-// Подключение к MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-mongoose.connection.on('connected', () => {
-  console.log('Connected to MongoDB');
-});
-
-mongoose.connection.on('error', (err) => {
-  console.error('Error connecting to MongoDB', err);
-});
-
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser());
-
-// Использование маршрутов
-app.use('/api/auth', authRoutes);
-app.use('/api/certificates', certificateRoutes);
-app.use('/api/consultations', consultationRoutes);
-app.use('/api/admin', adminRoutes);
-app.use('/api/surveys', surveyRoutes);
-app.use('/api/appointments', appointmentRoutes);
-
-app.use(express.static('build'));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
-});
-
 wss.on('connection', (ws) => {
-  console.log('WebSocket connection established');
+  console.log('New WebSocket connection');
+
   ws.on('message', (message) => {
-    console.log('Received:', message);
-    ws.send(`Echo: ${message}`);
+    console.log('received:', message);
+    // Обработка полученного сообщения
   });
+
   ws.on('close', () => {
     console.log('WebSocket connection closed');
   });
+
+  ws.send('Welcome to the WebSocket server');
 });
 
-const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
