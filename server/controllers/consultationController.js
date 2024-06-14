3f6d5c46b7c0840b85d4cdadb26a754ef865ff04
harddik25 <135815117+harddik25@users.noreply.github.com>
2024-06-15 01:52:55 +0200

    Update consultationController.js

--- server/controllers/consultationController.js
+++ server/controllers/consultationController.js
@@ -2,23 +2,33 @@ const Consultation = require('../models/Consultation');
 
 exports.scheduleAppointment = async (req, res) => {
   try {
-    const { date, time } = req.body;
+    const { date, time, patientName } = req.body;
 
     // Проверьте, что данные переданы корректно
-    if (!date || !time) {
-      return res.status(400).json({ message: 'Дата и время обязательны' });
+    if (!date || !time || !patientName) {
+      return res.status(400).json({ message: 'Дата, время и имя пациента обязательны' });
     }
 
     const newAppointment = new Consultation({
       date,
       time,
-      patientName: req.user ? req.user.username : 'Unknown'
+      patientName
     });
 
     await newAppointment.save();
     res.status(201).json({ success: true, appointment: newAppointment });
   } catch (error) {
     console.error('Ошибка при сохранении времени приема', error);
+    res.status(500).json({ message: 'Ошибка сервера', error: error.message });
+  }
+};
+
+exports.getAppointments = async (req, res) => {
+  try {
+    const appointments = await Consultation.find();
+    res.status(200).json({ appointments });
+  } catch (error) {
+    console.error('Ошибка при получении списка консультаций', error);
     res.status(500).json({ message: 'Ошибка сервера', error: error.message });
   }
 };
