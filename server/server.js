137e62a7319e5d8207c2c20ad3cecd2a378832a6
harddik25 <135815117+harddik25@users.noreply.github.com>
2024-06-14 17:15:40 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,25 +1,25 @@
+const app = require('./app');
 const http = require('http');
 const WebSocket = require('ws');
-const app = require('./app');
 const PORT = process.env.PORT || 5000;
 
 const server = http.createServer(app);
+
 const wss = new WebSocket.Server({ server });
 
 wss.on('connection', (ws) => {
-  console.log('WebSocket connection established');
+  console.log('New WebSocket connection');
 
   ws.on('message', (message) => {
-    console.log('WebSocket message received:', message);
+    console.log('received:', message);
+    // Обработка полученного сообщения
   });
 
   ws.on('close', () => {
     console.log('WebSocket connection closed');
   });
 
-  ws.on('error', (error) => {
-    console.error('WebSocket error:', error);
-  });
+  ws.send('Welcome to the WebSocket server');
 });
 
 server.listen(PORT, () => {
