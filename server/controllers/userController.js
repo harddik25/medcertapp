78c897e49b69d4855fcaf1ef17f3eb009394272e
harddik25 <135815117+harddik25@users.noreply.github.com>
2024-06-15 00:53:49 +0200

    Update userController.js

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,19 +1,29 @@
-const User = require('../models/User');
+const { MongoClient } = require('mongodb');
+const uri = process.env.MONGO_URI; // Убедитесь, что MONGO_URI правильно настроен в .env файле
 
 exports.getUserByTelegramId = async (req, res) => {
   const { telegramId } = req.params;
   console.log(`Fetching user with Telegram ID: ${telegramId}`);
 
+  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
   try {
-    const user = await User.findOne({ telegramId });
+    await client.connect();
+    const database = client.db('your_database_name'); // Замените на имя вашей базы данных
+    const collection = database.collection('users'); // Замените на имя вашей коллекции
+    const user = await collection.findOne({ telegramId });
+
     if (!user) {
       console.log(`User not found for Telegram ID: ${telegramId}`);
       return res.status(404).json({ message: 'User not found' });
     }
+
     console.log(`User found: ${user}`);
     res.json({ role: user.role });
   } catch (error) {
     console.error('Error fetching user by Telegram ID:', error);
     res.status(500).json({ message: 'Server error', error: error.message });
+  } finally {
+    await client.close();
   }
 };
