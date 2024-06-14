bf8291a7e36a7ba8a94e0402a54f5d2828f26305
harddik25 <135815117+harddik25@users.noreply.github.com>
2024-06-14 14:21:16 +0200

    Create WebSocketComponent.js

---
+++ src/components/WebSocketComponent.js
@@ -0,0 +1,31 @@
+import React, { useEffect } from 'react';
+
+const WebSocketComponent = () => {
+  useEffect(() => {
+    const ws = new WebSocket('wss://medlevel.me/ws');
+
+    ws.onopen = () => {
+      console.log('WebSocket connection opened');
+    };
+
+    ws.onmessage = (event) => {
+      console.log('WebSocket message received:', event);
+    };
+
+    ws.onerror = (error) => {
+      console.error('WebSocket error:', error);
+    };
+
+    ws.onclose = () => {
+      console.log('WebSocket connection closed');
+    };
+
+    return () => {
+      ws.close();
+    };
+  }, []);
+
+  return <div>WebSocket Component</div>;
+};
+
+export default WebSocketComponent;
