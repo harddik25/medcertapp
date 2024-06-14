4f15eac17b8cbe716b15cdb570088bc08b1bccc7
harddik25 <135815117+harddik25@users.noreply.github.com>
2024-06-14 23:24:10 +0200

    Update app.js

--- server/app.js
+++ server/app.js
@@ -54,10 +54,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.resolve('/var/www/medlevel.me', 'build', 'index.html'));
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
 
 module.exports = app;
 
