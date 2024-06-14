336721766d4404b44d06f819ddf1e994a5171f90
harddik25 <135815117+harddik25@users.noreply.github.com>
2024-06-14 17:28:18 +0200

    Update app.js

--- server/app.js
+++ server/app.js
@@ -4,7 +4,6 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-// Импорт маршрутов
 const authRoutes = require('./routes/authRoutes');
 const certificateRoutes = require('./routes/certificateRoutes');
 const consultationRoutes = require('./routes/consultationRoutes');
@@ -14,8 +13,7 @@ const appointmentRoutes = require('./routes/appointmentRoutes');
 
 const app = express();
 
-// Подключение к MongoDB
-mongoose.connect('mongodb+srv://admin:Gonga!2599@medapp.gugm54i.mongodb.net/', {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
@@ -32,16 +30,24 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Использование маршрутов
 app.use('/api/auth', authRoutes);
 app.use('/api/certificates', certificateRoutes);
 app.use('/api/consultations', consultationRoutes);
 app.use('/api/admin', adminRoutes);
 app.use('/api/surveys', surveyRoutes);
 app.use('/api/appointments', appointmentRoutes);
+
+// Маршрут для обработки аутентификации через Telegram
 app.get('/auth', (req, res) => {
   const { id, first_name, last_name, username, photo_url, auth_date, hash } = req.query;
   // Проверьте hash и сохраните данные пользователя в вашей системе
-  res.redirect('/profile');
+  res.json({ id, first_name, last_name, username, photo_url, auth_date, hash });
 });
+
+app.use(express.static('build'));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+});
+
 module.exports = app;
