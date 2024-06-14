bf8db85cb9689c485af53dea515e7e5b421010bc
harddik25 <135815117+harddik25@users.noreply.github.com>
2024-06-14 20:06:48 +0200

    Update userRoutes.js

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { updateUserRole } = require('../controllers/userController');
-const { verifyToken, isAdmin } = require('../middleware/authMiddleware');
+const { verifyToken, isAdmin } = require('../authMiddleware');
 
 router.put('/:id/role', verifyToken, isAdmin, updateUserRole);
 
