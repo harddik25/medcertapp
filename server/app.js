cbddb02abf42e91baf47f687747cf9053fc90d40
harddik25 <135815117+harddik25@users.noreply.github.com>
2024-06-15 00:13:35 +0200

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
-const path = require('path');
 
 // Импорт маршрутов
 const authRoutes = require('./routes/authRoutes');
@@ -13,29 +13,39 @@ const consultationRoutes = require('./routes/consultationRoutes');
 const adminRoutes = require('./routes/adminRoutes');
 const surveyRoutes = require('./routes/surveyRoutes');
 const appointmentRoutes = require('./routes/appointmentRoutes');
-const userRoutes = require('./routes/userRoutes'); // Добавлен новый маршрут
+const userRoutes = require('./routes/userRoutes');
 
+// Инициализация приложения
 const app = express();
 
-
-// Подключение к MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-  // Обновленные параметры подключения
-});
-
-mongoose.connection.on('connected', () => {
-  console.log('Connected to MongoDB');
-});
-
-mongoose.connection.on('error', (err) => {
-  console.error('Error connecting to MongoDB', err);
-});
-
-
+// Настройка middleware
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+// Подключение к MongoDB с использованием MongoClient
+const uri = process.env.MONGO_URI; // Убедитесь, что MONGO_URI правильно настроен в .env файле
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Подключение клиента к серверу
+    await client.connect();
+    console.log("Successfully connected to MongoDB!");
+  } catch (err) {
+    console.error("Error connecting to MongoDB:", err);
+  }
+}
+
+run().catch(console.dir);
 
 // Использование маршрутов
 app.use('/api/auth', authRoutes);
@@ -44,13 +54,15 @@ app.use('/api/consultations', consultationRoutes);
 app.use('/api/admin', adminRoutes);
 app.use('/api/surveys', surveyRoutes);
 app.use('/api/appointments', appointmentRoutes);
-app.use('/api/users', userRoutes); // Использование нового маршрута
+app.use('/api/users', userRoutes);
 
 app.use(express.static('build'));
 
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
 });
+
+
 
 module.exports = app;
 
