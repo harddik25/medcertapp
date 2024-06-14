6db8bd9b4d90f45d077d6d977af9c9a68b34543e
harddik25 <135815117+harddik25@users.noreply.github.com>
2024-06-15 01:11:27 +0200

    Update ProtectedRoute.js

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,15 +1,10 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
-import { default as jwtDecode } from 'jwt-decode';  // Используем именованный импорт правильно
 
 const ProtectedRoute = ({ children, allowedRoles }) => {
-  const token = localStorage.getItem('token');
-  if (!token) {
-    return <Navigate to="/login" />;
-  }
+  const user = JSON.parse(localStorage.getItem('telegramUser'));
 
-  const { role } = jwtDecode(token);  // Используем jwtDecode для декодирования токена
-  if (!allowedRoles.includes(role)) {
+  if (!user || !allowedRoles.includes(user.role)) {
     return <Navigate to="/profile" />;
   }
 
