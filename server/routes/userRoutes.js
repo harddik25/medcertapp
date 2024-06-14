07054bbdcd86bfcc0762dee32171a2ce906f2c70
harddik25 <135815117+harddik25@users.noreply.github.com>
2024-06-15 00:03:04 +0200

    Update userRoutes.js

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const { updateUserRoleByTelegramId } = require('../controllers/userController');
-const { verifyAdmin } = require('../authMiddleware');
+const { getUserByTelegramId } = require('../controllers/userController');
 
-router.put('/role', verifyAdmin, updateUserRoleByTelegramId);
+router.get('/role/:telegramId', getUserByTelegramId);
 
 module.exports = router;
