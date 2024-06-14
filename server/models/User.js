fc3131a2e293120ac3f6a50ca8c442827cfa808f
harddik25 <135815117+harddik25@users.noreply.github.com>
2024-06-14 20:00:16 +0200

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  telegramId: { type: String, unique: true, required: true },
+  telegramId: { type: String, required: true, unique: true },
   firstName: String,
   lastName: String,
   username: String,
   photoUrl: String,
+  role: { type: String, enum: ['user', 'doctor', 'admin'], default: 'user' }, // добавляем поле role
 });
 
 module.exports = mongoose.model('User', userSchema);
