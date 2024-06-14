07d620178c9811ab1245c7ada092e3b4ed6abac1
harddik25 <135815117+harddik25@users.noreply.github.com>
2024-06-15 01:44:10 +0200

    Update ProtectedRoute.js

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -4,6 +4,9 @@ import { Navigate } from 'react-router-dom';
 const ProtectedRoute = ({ children, allowedRoles }) => {
   const user = JSON.parse(localStorage.getItem('telegramUser'));
 
+  console.log('ProtectedRoute - User:', user); // Отладочное сообщение
+  console.log('Allowed roles:', allowedRoles); // Отладочное сообщение
+
   if (!user || !allowedRoles.includes(user.role)) {
     return <Navigate to="/profile" />;
   }
