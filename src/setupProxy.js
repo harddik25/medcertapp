472289210aa62b1995ecbc99384b5055ca04a6d1
harddik25 <135815117+harddik25@users.noreply.github.com>
2024-06-14 14:20:55 +0200

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,15 +1,1 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
 
-module.exports = function(app) {
-  app.use(
-    '/ws',
-    createProxyMiddleware({
-      target: 'http://localhost:5000', // Ваш backend
-      changeOrigin: true,
-      ws: true,
-      pathRewrite: {
-        '^/ws': '', // Убедитесь, что путь правильно переписывается
-      },
-    })
-  );
-};
