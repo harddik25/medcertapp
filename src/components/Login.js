d89e50d9dd0a348ce56388267061c46c19c8ceb0
harddik25 <135815117+harddik25@users.noreply.github.com>
2024-06-14 16:07:30 +0200

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,13 +5,16 @@ const Login = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
+    // Проверьте, доступен ли объект Telegram Web App API
     if (window.Telegram && window.Telegram.WebApp) {
-      const user = window.Telegram.WebApp.initDataUnsafe?.user;
-
+      const user = window.Telegram.WebApp.initDataUnsafe.user;
+      
+      // Если пользователь уже авторизован
       if (user) {
         localStorage.setItem('telegramUser', JSON.stringify(user));
         navigate('/language');
       } else {
+        // Инициализация виджета Telegram для авторизации
         const script = document.createElement('script');
         script.src = "https://telegram.org/js/telegram-widget.js?7";
         script.async = true;
@@ -47,3 +50,4 @@ const Login = () => {
 };
 
 export default Login;
+
