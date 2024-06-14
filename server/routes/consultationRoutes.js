248faf76f7384134b0c0de0edea0cba758c7173e
harddik25 <135815117+harddik25@users.noreply.github.com>
2024-06-15 01:59:05 +0200

    Update consultationRoutes.js

--- server/routes/consultationRoutes.js
+++ server/routes/consultationRoutes.js
@@ -4,5 +4,7 @@ const consultationController = require('../controllers/consultationController');
 
 router.get('/appointments', consultationController.getAppointments);
 router.post('/schedule', consultationController.scheduleAppointment);
+router.post('/add-free-slot', consultationController.addFreeSlot);
+router.get('/free-slots', consultationController.getFreeSlots);
 
 module.exports = router;
