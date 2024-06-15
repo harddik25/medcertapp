5a5aca621d48a290457f1a4fa9d69fe54435ee2f
harddik25 <135815117+harddik25@users.noreply.github.com>
2024-06-15 13:32:51 +0200

    Update DocumentUpload.js

--- src/components/DocumentUpload.js
+++ src/components/DocumentUpload.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Container, Box, Typography, Button, CssBaseline, Paper, TextField, MenuItem } from '@mui/material';
 import { styled } from '@mui/system';
-import CannabisBackground from '../logos/cannabis-background.jpeg';
+import CannabisBackground from './cannabis-background.webp';
 import { useNavigate } from 'react-router-dom';
 
 const Background = styled('div')({
