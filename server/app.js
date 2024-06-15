57950f893a52f322d29116145173b7f63b6afb6e
harddik25 <135815117+harddik25@users.noreply.github.com>
2024-06-15 03:34:39 +0200

    Update app.js

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const mongoose = require('mongoose');
 const path = require('path');
 require('dotenv').config();
 
@@ -15,7 +15,6 @@ const surveyRoutes = require('./routes/surveyRoutes');
 const appointmentRoutes = require('./routes/appointmentRoutes');
 const userRoutes = require('./routes/userRoutes');
 const documentRoutes = require('./routes/documentRoutes');
-
 // Инициализация приложения
 const app = express();
 
@@ -25,70 +24,38 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-// Подключение к MongoDB с использованием MongoClient
-const uri = process.env.MONGO_URI; // Убедитесь, что MONGO_URI правильно настроен в .env файле
+// Подключение к MongoDB с использованием Mongoose
+const uri = process.env.MONGO_URI;
 
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log("Successfully connected to MongoDB!");
+
+  // Настройка маршрутов после успешного подключения
+  app.use('/api/auth', authRoutes);
+  app.use('/api/certificates', certificateRoutes);
+  app.use('/api/consultations', consultationRoutes);
+  app.use('/api/admin', adminRoutes);
+  app.use('/api/surveys', surveyRoutes);
+  app.use('/api/appointments', appointmentRoutes);
+  app.use('/api/users', userRoutes);
+  app.use('/api/documents', documentRoutes);
+  // Настройка пути к статическим файлам
+  app.use(express.static('/var/www/medlevel.me'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve('/var/www/medlevel.me', 'index.html'));
+  });
+
+  // Запуск сервера
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}).catch(err => {
+  console.error("Error connecting to MongoDB:", err);
 });
-
-async function initializeDatabase() {
-  try {
-    const database = client.db('medapp');
-
-    // Проверка и создание коллекций, если они не существуют
-    const collections = ['users', 'appointments', 'certificates', 'consultations', 'surveys'];
-    const existingCollections = await database.listCollections().toArray();
-    const existingCollectionNames = existingCollections.map(col => col.name);
-
-    for (const collection of collections) {
-      if (!existingCollectionNames.includes(collection)) {
-        await database.createCollection(collection);
-        console.log(`Collection ${collection} created!`);
-      } else {
-        console.log(`Collection ${collection} already exists.`);
-      }
-    }
-  } catch (error) {
-    console.error('Ошибка при инициализации базы данных', error);
-  }
-}
-
-async function run() {
-  try {
-    // Подключение клиента к серверу
-    await client.connect();
-    console.log("Successfully connected to MongoDB!");
-
-    // Инициализация базы данных
-    await initializeDatabase();
-
-    // Настройка маршрутов после успешного подключения
-    app.use('/api/auth', authRoutes);
-    app.use('/api/certificates', certificateRoutes);
-    app.use('/api/consultations', consultationRoutes);
-    app.use('/api/admin', adminRoutes);
-    app.use('/api/surveys', surveyRoutes);
-    app.use('/api/appointments', appointmentRoutes);
-    app.use('/api/users', userRoutes);
-    app.use('/api/documents', documentRoutes);
-
-    // Настройка пути к статическим файлам
-    app.use(express.static('/var/www/medlevel.me'));
-
-    app.get('*', (req, res) => {
-      res.sendFile(path.resolve('/var/www/medlevel.me', 'index.html'));
-    });
-
-  } catch (err) {
-    console.error("Error connecting to MongoDB:", err);
-  }
-}
-
-run().catch(console.dir);
 
 module.exports = app;
