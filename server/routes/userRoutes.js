2bf9a2ab0f03e59c5573845930218e3cafbdd45d
harddik25 <135815117+harddik25@users.noreply.github.com>
2024-06-15 15:08:00 +0200

    Update userRoutes.js

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
-const consultationController = require('../controllers/consultationController');
 
 router.get('/role/:telegramId', userController.getUserRoleByTelegramId);
-router.get('/user/:userId', userController.getUserById); // Добавлено для получения информации о пользователе
-router.get('/consultations/user/:userId', consultationController.getAppointmentByUserId);
+router.get('/:userId', userController.getUserById);
 
 module.exports = router;
