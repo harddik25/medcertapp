90eb08aea3fed8fdc85e3281dde350756bdaf5f9
harddik25 <135815117+harddik25@users.noreply.github.com>
2024-06-15 13:32:59 +0200

    Update LanguageSelection.js

--- src/components/LanguageSelection.js
+++ src/components/LanguageSelection.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Container, Box, Typography, Button, CssBaseline, Paper } from '@mui/material';
 import { styled } from '@mui/system';
 import { useNavigate } from 'react-router-dom';
-import CannabisBackground from '../logos/cannabis-background.jpeg'; // Замените на путь к вашему фоновому изображению
+import CannabisBackground from './cannabis-background.webp'; // Замените на путь к вашему фоновому изображению
 
 const Background = styled('div')({
   display: 'flex',
