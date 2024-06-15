a31e9b9771573962d8955e7f87d58f4500d296c4
harddik25 <135815117+harddik25@users.noreply.github.com>
2024-06-15 15:07:21 +0200

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -1,12 +1,30 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  telegramId: { type: String, required: true, unique: true },
-  firstName: String,
-  lastName: String,
-  username: String,
-  photoUrl: String,
-  role: { type: String, enum: ['user', 'doctor', 'admin'], default: 'user' },
+  telegramId: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  photoUrl: {
+    type: String
+  },
+  role: {
+    type: String,
+    enum: ['user', 'doctor', 'admin'],
+    default: 'user'
+  },
+  username: {
+    type: String
+  }
 });
 
 module.exports = mongoose.model('User', userSchema);
