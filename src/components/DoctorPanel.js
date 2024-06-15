3516d12e9148b9ce6fde118eca76826935ac6f4c
harddik25 <135815117+harddik25@users.noreply.github.com>
2024-06-15 14:27:45 +0200

    Update DoctorPanel.js

--- src/components/DoctorPanel.js
+++ src/components/DoctorPanel.js
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { Container, Box, Typography, Button, CssBaseline, Paper, TextField, List, ListItem, ListItemText, IconButton, Dialog, DialogTitle, DialogContent, DialogActions } from '@mui/material';
+import { Container, Box, Typography, Button, CssBaseline, Paper, List, ListItem, ListItemText, Dialog, DialogActions, DialogContent, DialogTitle, TextField } from '@mui/material';
 import { styled } from '@mui/system';
-import DeleteIcon from '@mui/icons-material/Delete';
-import CannabisBackground from './cannabis-background.webp'; // Замените на путь к вашему фоновому изображению
+import CannabisBackground from '../logos/cannabis-background.jpeg';
 
 const Background = styled('div')({
   display: 'flex',
@@ -14,46 +13,46 @@ const Background = styled('div')({
 });
 
 const DoctorPanel = () => {
-  const [appointments, setAppointments] = useState([]);
-  const [freeSlots, setFreeSlots] = useState([]);
-  const [open, setOpen] = useState(false);
+  const [openAddSlotDialog, setOpenAddSlotDialog] = useState(false);
   const [date, setDate] = useState('');
   const [time, setTime] = useState('');
+  const [appointments, setAppointments] = useState([]);
+  const [freeSlots, setFreeSlots] = useState([]);
 
   useEffect(() => {
-    const fetchAppointments = async () => {
-      try {
-        const response = await fetch('https://medlevel.me/api/consultations/appointments');
-        const data = await response.json();
-        setAppointments(data.appointments);
-      } catch (error) {
-        console.error('Ошибка при получении списка консультаций', error);
-      }
-    };
-
-    const fetchFreeSlots = async () => {
-      try {
-        const response = await fetch('https://medlevel.me/api/consultations/free-slots');
-        const data = await response.json();
-        setFreeSlots(data.slots);
-      } catch (error) {
-        console.error('Ошибка при получении списка свободного времени', error);
-      }
-    };
-
     fetchAppointments();
     fetchFreeSlots();
   }, []);
 
-  const handleClickOpen = () => {
-    setOpen(true);
+  const fetchAppointments = async () => {
+    try {
+      const response = await fetch('https://medlevel.me/api/consultations/appointments');
+      const data = await response.json();
+      setAppointments(data.appointments);
+    } catch (error) {
+      console.error('Ошибка при получении списка консультаций', error);
+    }
   };
 
-  const handleClose = () => {
-    setOpen(false);
+  const fetchFreeSlots = async () => {
+    try {
+      const response = await fetch('https://medlevel.me/api/consultations/free-slots');
+      const data = await response.json();
+      setFreeSlots(data.slots);
+    } catch (error) {
+      console.error('Ошибка при получении списка свободного времени', error);
+    }
   };
 
-  const handleSave = async () => {
+  const handleAddSlotOpen = () => {
+    setOpenAddSlotDialog(true);
+  };
+
+  const handleAddSlotClose = () => {
+    setOpenAddSlotDialog(false);
+  };
+
+  const handleAddSlotSave = async () => {
     try {
       const response = await fetch('https://medlevel.me/api/consultations/add-free-slot', {
         method: 'POST',
@@ -65,31 +64,28 @@ const DoctorPanel = () => {
       const data = await response.json();
       if (data.success) {
         setFreeSlots([...freeSlots, data.freeSlot]);
-        setOpen(false);
+        setOpenAddSlotDialog(false);
       } else {
-        console.error('Ошибка при сохранении времени приема');
+        console.error('Ошибка при добавлении свободного времени');
       }
     } catch (error) {
-      console.error('Ошибка при сохранении времени приема', error);
+      console.error('Ошибка при добавлении свободного времени', error);
     }
   };
 
-  const handleDelete = async (id) => {
+  const handleDeleteSlot = async (slotId) => {
     try {
-      const response = await fetch(`https://medlevel.me/api/consultations/free-slots/${id}`, {
+      const response = await fetch(`https://medlevel.me/api/consultations/free-slots/${slotId}`, {
         method: 'DELETE',
-        headers: {
-          'Content-Type': 'application/json',
-        },
       });
       const data = await response.json();
       if (data.success) {
-        setFreeSlots(freeSlots.filter(slot => slot._id !== id));
+        setFreeSlots(freeSlots.filter(slot => slot._id !== slotId));
       } else {
-        console.error('Ошибка при удалении времени приема');
+        console.error('Ошибка при удалении свободного времени');
       }
     } catch (error) {
-      console.error('Ошибка при удалении времени приема', error);
+      console.error('Ошибка при удалении свободного времени', error);
     }
   };
 
@@ -98,32 +94,23 @@ const DoctorPanel = () => {
       <Container component="main" maxWidth="md">
         <CssBaseline />
         <Paper elevation={3} sx={{ padding: 3, backgroundColor: 'rgba(255, 255, 255, 0.8)' }}>
-          <Box
-            sx={{
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-            }}
-          >
+          <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
             <Typography component="h1" variant="h5" sx={{ color: '#388e3c' }}>
               Кабинет врача
             </Typography>
-            <Box sx={{ mt: 1, width: '100%' }}>
-              <Button variant="contained" color="primary" onClick={handleClickOpen} sx={{ mb: 2 }}>
-                Добавить время приема
+            <Box sx={{ mt: 2, width: '100%' }}>
+              <Button variant="contained" color="primary" onClick={handleAddSlotOpen} sx={{ mb: 2 }}>
+                Добавить свободное время
               </Button>
+              <Button variant="contained" color="secondary" onClick={fetchAppointments} sx={{ mb: 2 }}>
+                Просмотр записей пользователей
+              </Button>
+              <Button variant="contained" color="secondary" onClick={fetchFreeSlots} sx={{ mb: 2 }}>
+                Просмотр свободных слотов
+              </Button>
+              <Typography variant="h6" sx={{ mt: 2 }}>Записи пользователей</Typography>
               <List>
-                {freeSlots.map((slot, index) => (
-                  <ListItem key={index}>
-                    <ListItemText primary={`${slot.date} ${slot.time}`} sx={{ color: '#388e3c' }} />
-                    <IconButton edge="end" aria-label="delete" onClick={() => handleDelete(slot._id)}>
-                      <DeleteIcon />
-                    </IconButton>
-                  </ListItem>
-                ))}
-              </List>
-              <List>
-                {appointments.map((appointment, index) => (
+                {appointments.length > 0 ? appointments.map((appointment, index) => (
                   <ListItem key={index}>
                     <ListItemText
                       primary={`${appointment.date} ${appointment.time}`}
@@ -131,22 +118,38 @@ const DoctorPanel = () => {
                       sx={{ color: '#388e3c' }}
                     />
                   </ListItem>
-                ))}
+                )) : (
+                  <Typography>Нет записей пользователей</Typography>
+                )}
+              </List>
+              <Typography variant="h6" sx={{ mt: 2 }}>Свободные слоты</Typography>
+              <List>
+                {freeSlots.length > 0 ? freeSlots.map((slot, index) => (
+                  <ListItem key={index}>
+                    <ListItemText
+                      primary={`${slot.date} ${slot.time}`}
+                      sx={{ color: '#388e3c' }}
+                    />
+                    <Button variant="contained" color="secondary" onClick={() => handleDeleteSlot(slot._id)}>
+                      Удалить
+                    </Button>
+                  </ListItem>
+                )) : (
+                  <Typography>Свободных слотов нет</Typography>
+                )}
               </List>
             </Box>
           </Box>
         </Paper>
-        <Dialog open={open} onClose={handleClose}>
-          <DialogTitle>Добавить время приема</DialogTitle>
+        <Dialog open={openAddSlotDialog} onClose={handleAddSlotClose}>
+          <DialogTitle>Добавить свободное время</DialogTitle>
           <DialogContent>
             <TextField
               margin="dense"
               label="Дата"
               type="date"
               fullWidth
-              InputLabelProps={{
-                shrink: true,
-              }}
+              InputLabelProps={{ shrink: true }}
               value={date}
               onChange={(e) => setDate(e.target.value)}
             />
@@ -155,18 +158,16 @@ const DoctorPanel = () => {
               label="Время"
               type="time"
               fullWidth
-              InputLabelProps={{
-                shrink: true,
-              }}
+              InputLabelProps={{ shrink: true }}
               value={time}
               onChange={(e) => setTime(e.target.value)}
             />
           </DialogContent>
           <DialogActions>
-            <Button onClick={handleClose} color="primary">
+            <Button onClick={handleAddSlotClose} color="primary">
               Отмена
             </Button>
-            <Button onClick={handleSave} color="primary">
+            <Button onClick={handleAddSlotSave} color="primary">
               Сохранить
             </Button>
           </DialogActions>
@@ -177,5 +178,4 @@ const DoctorPanel = () => {
 };
 
 export default DoctorPanel;
-
 
