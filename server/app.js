b0014417d4ce1f045c8bb6c923db7347e6f8654b
harddik25 <135815117+harddik25@users.noreply.github.com>
2024-06-15 03:35:22 +0200

    Update app.js

--- server/app.js
+++ server/app.js
@@ -49,11 +49,7 @@ mongoose.connect(uri, {
     res.sendFile(path.resolve('/var/www/medlevel.me', 'index.html'));
   });
 
-  // Запуск сервера
-  const PORT = process.env.PORT || 5000;
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-  });
+  
 }).catch(err => {
   console.error("Error connecting to MongoDB:", err);
 });
