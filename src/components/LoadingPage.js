82d19bf3cdab21fafd50742463c4319d43eadf90
harddik25 <135815117+harddik25@users.noreply.github.com>
2024-06-15 13:32:02 +0200

    Update LoadingPage.js

--- src/components/LoadingPage.js
+++ src/components/LoadingPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Container, Box, Typography, CircularProgress } from '@mui/material';
 import { styled } from '@mui/system';
-import CompanyLogo from '../logos/MedLevel.jpg'; // Замените на путь к вашему логотипу
+import CompanyLogo from './MedLevel.webp'; // Замените на путь к вашему логотипу
 import CannabisBackground from './cannabis-background.webp'; // Замените на путь к вашему фоновому изображению
 
 const Background = styled('div')({
