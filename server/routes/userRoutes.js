b53bf775994cf30beccbe50c0b2e237478c8978f
harddik25 <135815117+harddik25@users.noreply.github.com>
2024-06-16 03:25:30 +0200

    Update userRoutes.js

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -3,6 +3,5 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 
 router.get('/role/:telegramId', userController.getUserRoleByTelegramId);
-router.get('/:userId', userController.getUserById);
 
 module.exports = router;
