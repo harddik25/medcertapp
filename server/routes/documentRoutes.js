8ef0d5144950ea263390bf82f3b0e6447aa6ea94
harddik25 <135815117+harddik25@users.noreply.github.com>
2024-06-16 03:46:04 +0200

    Update documentRoutes.js

--- server/routes/documentRoutes.js
+++ server/routes/documentRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const multer = require('multer');
 const router = express.Router();
-const { uploadDocument } = require('../controllers/documentController');
+const multer = require('multer');
+const documentController = require('../controllers/documentController');
 
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
-router.post('/upload', upload.single('document'), uploadDocument);
+router.post('/upload', upload.single('document'), documentController.uploadDocument);
 
 module.exports = router;
