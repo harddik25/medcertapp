98a3aa29b14768cb062996ad0fd014f675e269dc
harddik25 <135815117+harddik25@users.noreply.github.com>
2024-06-17 12:48:42 +0200

    Update MainSurvey.js

--- src/components/MainSurvey.js
+++ src/components/MainSurvey.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Container, Box, Typography, Button, CssBaseline, Paper, TextField, MenuItem } from '@mui/material';
+import { Container, Box, Typography, Button, CssBaseline, Paper, TextField, MenuItem, IconButton, FormControl, FormLabel, RadioGroup, FormControlLabel, Radio } from '@mui/material';
 import { createTheme, ThemeProvider, styled } from '@mui/material/styles';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import CannabisBackground from './cannabis-background.webp';
 
 const theme = createTheme();
@@ -13,15 +14,22 @@ const Background = styled('div')({
   minHeight: '100vh',
   backgroundImage: `url(${CannabisBackground})`,
   backgroundSize: 'cover',
-  paddingTop: '20px', // Добавляем отступ сверху
-  paddingBottom: '20px', // Добавляем отступ снизу
+  paddingTop: '20px',
+  paddingBottom: '20px',
 });
 
 const ScrollablePaper = styled(Paper)({
-  maxHeight: 'calc(100vh - 40px)', // Уменьшаем высоту Paper, чтобы он умещался на экране с отступами
-  overflowY: 'auto', // Добавляем прокрутку по вертикали
+  maxHeight: 'calc(100vh - 40px)',
+  overflowY: 'auto',
   padding: 3,
   backgroundColor: 'rgba(255, 255, 255, 0.8)',
+});
+
+const Header = styled('div')({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  width: '100%',
 });
 
 const MainSurvey = () => {
@@ -29,11 +37,7 @@ const MainSurvey = () => {
   const [surveyData, setSurveyData] = useState({
     generalHealth: '',
     healthComparison: '',
-    physicalActivities: {
-      running: '',
-      lifting: '',
-      walking: '',
-    },
+    physicalActivities: {},
     emotionalProblems: '',
     socialActivities: '',
     bodyPain: '',
@@ -47,9 +51,24 @@ const MainSurvey = () => {
     setSurveyData({ ...surveyData, [name]: value });
   };
 
+  const handlePhysicalActivityChange = (e) => {
+    const { name, value } = e.target;
+    setSurveyData({
+      ...surveyData,
+      physicalActivities: {
+        ...surveyData.physicalActivities,
+        [name]: value,
+      },
+    });
+  };
+
   const handleSubmit = () => {
     // Обработка данных опросника и переход к оплате и выбору консультации
     navigate('/consultation');
+  };
+
+  const handleBackClick = () => {
+    navigate('/profile');
   };
 
   return (
@@ -58,17 +77,23 @@ const MainSurvey = () => {
         <Container component="main" maxWidth="sm">
           <CssBaseline />
           <ScrollablePaper elevation={3}>
+            <Header>
+              <IconButton onClick={handleBackClick} sx={{ alignSelf: 'flex-start' }}>
+                <ArrowBackIcon style={{ color: '#388e3c' }} />
+              </IconButton>
+              <Typography component="h1" variant="h5" sx={{ color: '#388e3c', flexGrow: 1, textAlign: 'center' }}>
+                Health Survey
+              </Typography>
+              <div style={{ width: '30px', height: '30px' }}></div>
+            </Header>
             <Box
               sx={{
-                marginTop: 8,
+                marginTop: 2,
                 display: 'flex',
                 flexDirection: 'column',
                 alignItems: 'center',
               }}
             >
-              <Typography component="h1" variant="h5" sx={{ color: '#388e3c', marginBottom: 2 }}>
-                Health Survey
-              </Typography>
               <TextField
                 select
                 label="In general, would you say that your health is:"
@@ -101,114 +126,36 @@ const MainSurvey = () => {
                 <MenuItem value="somewhatWorseNow">Somewhat worse now than one year ago</MenuItem>
                 <MenuItem value="muchWorseNow">Much worse now than one year ago</MenuItem>
               </TextField>
-              <TextField
-                select
-                label="Does your health now limit you in physical activities (e.g., running, lifting, walking)?"
-                name="physicalActivities.running"
-                value={surveyData.physicalActivities.running}
-                onChange={handleInputChange}
-                fullWidth
-                margin="normal"
-                variant="outlined"
-              >
-                <MenuItem value="veryLimited">Very limited</MenuItem>
-                <MenuItem value="somewhatLimited">Somewhat limited</MenuItem>
-                <MenuItem value="notLimited">Not limited</MenuItem>
-              </TextField>
-              <TextField
-                select
-                label="During the last 4 weeks, have you had any emotional problems (e.g., feeling depressed or anxious)?"
-                name="emotionalProblems"
-                value={surveyData.emotionalProblems}
-                onChange={handleInputChange}
-                fullWidth
-                margin="normal"
-                variant="outlined"
-              >
-                <MenuItem value="yes">Yes</MenuItem>
-                <MenuItem value="no">No</MenuItem>
-              </TextField>
-              <TextField
-                select
-                label="During the past 4 weeks, how much have your physical or emotional problems interfered with your social activities?"
-                name="socialActivities"
-                value={surveyData.socialActivities}
-                onChange={handleInputChange}
-                fullWidth
-                margin="normal"
-                variant="outlined"
-              >
-                <MenuItem value="allTheTime">All the time</MenuItem>
-                <MenuItem value="mostOfTheTime">Most of the time</MenuItem>
-                <MenuItem value="someOfTheTime">Some of the time</MenuItem>
-                <MenuItem value="aLittleOfTheTime">A little of the time</MenuItem>
-                <MenuItem value="noneOfTheTime">None of the time</MenuItem>
-              </TextField>
-              <TextField
-                select
-                label="How much body pain have you had in the last 4 weeks?"
-                name="bodyPain"
-                value={surveyData.bodyPain}
-                onChange={handleInputChange}
-                fullWidth
-                margin="normal"
-                variant="outlined"
-              >
-                <MenuItem value="none">None</MenuItem>
-                <MenuItem value="veryMild">Very mild</MenuItem>
-                <MenuItem value="mild">Mild</MenuItem>
-                <MenuItem value="moderate">Moderate</MenuItem>
-                <MenuItem value="severe">Severe</MenuItem>
-                <MenuItem value="verySevere">Very severe</MenuItem>
-              </TextField>
-              <TextField
-                select
-                label="During the last 4 weeks, how much did pain interfere with your normal work (including both work outside the home and at home)?"
-                name="painInterference"
-                value={surveyData.painInterference}
-                onChange={handleInputChange}
-                fullWidth
-                margin="normal"
-                variant="outlined"
-              >
-                <MenuItem value="notAtAll">Not at all</MenuItem>
-                <MenuItem value="aLittleBit">A little bit</MenuItem>
-                <MenuItem value="moderately">Moderately</MenuItem>
-                <MenuItem value="quiteABit">Quite a bit</MenuItem>
-                <MenuItem value="extremely">Extremely</MenuItem>
-              </TextField>
-              <TextField
-                select
-                label="How much of the time during the last 4 weeks did you feel full of energy?"
-                name="energyLevels"
-                value={surveyData.energyLevels}
-                onChange={handleInputChange}
-                fullWidth
-                margin="normal"
-                variant="outlined"
-              >
-                <MenuItem value="allTheTime">All the time</MenuItem>
-                <MenuItem value="mostOfTheTime">Most of the time</MenuItem>
-                <MenuItem value="someOfTheTime">Some of the time</MenuItem>
-                <MenuItem value="aLittleOfTheTime">A little of the time</MenuItem>
-                <MenuItem value="noneOfTheTime">None of the time</MenuItem>
-              </TextField>
-              <TextField
-                select
-                label="During the past 4 weeks, how much of the time have you been a happy person?"
-                name="happiness"
-                value={surveyData.happiness}
-                onChange={handleInputChange}
-                fullWidth
-                margin="normal"
-                variant="outlined"
-              >
-                <MenuItem value="allTheTime">All the time</MenuItem>
-                <MenuItem value="mostOfTheTime">Most of the time</MenuItem>
-                <MenuItem value="someOfTheTime">Some of the time</MenuItem>
-                <MenuItem value="aLittleOfTheTime">A little of the time</MenuItem>
-                <MenuItem value="noneOfTheTime">None of the time</MenuItem>
-              </TextField>
+              <Typography variant="body1" sx={{ marginTop: 2, textAlign: 'left', width: '100%' }}>
+                The following articles are about activities you can do during a typical day. Does your health now limit you in these activities? If so, how much? PLEASE CIRCLE THE NUMBER THAT BEST DESCRIBES YOUR ANSWER.
+              </Typography>
+              <Box sx={{ width: '100%', marginTop: 2 }}>
+                <FormControl component="fieldset" sx={{ width: '100%' }}>
+                  <FormLabel component="legend">Vigorous activities, such as running, lifting heavy objects, participating in strenuous sports.</FormLabel>
+                  <RadioGroup
+                    row
+                    name="vigorousActivities"
+                    value={surveyData.physicalActivities.vigorousActivities}
+                    onChange={handlePhysicalActivityChange}
+                  >
+                    <FormControlLabel value="veryLimited" control={<Radio />} label="1 (Very limited)" />
+                    <FormControlLabel value="somewhatLimited" control={<Radio />} label="2 (Somewhat limited)" />
+                    <FormControlLabel value="notLimited" control={<Radio />} label="3 (Not limited)" />
+                  </RadioGroup>
+                  <FormLabel component="legend">Moderate activities, such as moving a table, pushing a vacuum, going bowling or playing golf.</FormLabel>
+                  <RadioGroup
+                    row
+                    name="moderateActivities"
+                    value={surveyData.physicalActivities.moderateActivities}
+                    onChange={handlePhysicalActivityChange}
+                  >
+                    <FormControlLabel value="veryLimited" control={<Radio />} label="1 (Very limited)" />
+                    <FormControlLabel value="somewhatLimited" control={<Radio />} label="2 (Somewhat limited)" />
+                    <FormControlLabel value="notLimited" control={<Radio />} label="3 (Not limited)" />
+                  </RadioGroup>
+                  {/* Add other physical activities in similar way */}
+                </FormControl>
+              </Box>
               <Button
                 fullWidth
                 variant="contained"
