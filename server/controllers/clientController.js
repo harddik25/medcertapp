d8d29fe65ef5d46b8b46132835d2c413354c3a1a
harddik25 <135815117+harddik25@users.noreply.github.com>
2024-06-19 21:57:27 +0200

    Update clientController.js

--- server/controllers/clientController.js
+++ server/controllers/clientController.js
@@ -39,7 +39,7 @@ exports.getClientInfo = async (req, res) => {
         secure: false,
       });
 
-      const remotePath = `/var/www/user4806313/data/${patientId}/`;
+      const remotePath = `/var/www/user4806313/data/${patientId}/${documentName}`;
       documents = await listFTPDocuments(client, remotePath);
 
     } catch (error) {
