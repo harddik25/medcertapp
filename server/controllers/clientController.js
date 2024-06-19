7eb5967ab4a61be50d58a3681c8fcfd4e02e02ce
harddik25 <135815117+harddik25@users.noreply.github.com>
2024-06-19 21:52:50 +0200

    Update clientController.js

--- server/controllers/clientController.js
+++ server/controllers/clientController.js
@@ -1,6 +1,18 @@
 const Consultation = require('../models/Consultation');
 const Survey = require('../models/Survey');
-// Предполагая, что у вас есть модель для хранения документов
+const ftp = require('basic-ftp');
+const path = require('path');
+const fs = require('fs');
+
+async function listFTPDocuments(client, remotePath) {
+  try {
+    const fileList = await client.list(remotePath);
+    return fileList.map(file => file.name);
+  } catch (error) {
+    console.error('Error listing FTP documents:', error);
+    throw error;
+  }
+}
 
 exports.getClientInfo = async (req, res) => {
   try {
@@ -12,14 +24,36 @@ exports.getClientInfo = async (req, res) => {
     // Получение информации о ответах на опросы клиента
     const surveys = await Survey.find({ userId: patientId });
 
+    // Установка соединения с FTP
+    const client = new ftp.Client();
+    client.ftp.verbose = true;
+    client.ftp.timeout = 0;
 
+    let documents = [];
+
+    try {
+      await client.access({
+        host: process.env.FTP_HOST,
+        user: process.env.FTP_USER,
+        password: process.env.FTP_PASSWORD,
+        secure: false,
+      });
+
+      const remotePath = `/var/www/user4806313/data/${patientId}`;
+      documents = await listFTPDocuments(client, remotePath);
+
+    } catch (error) {
+      console.error('Error accessing FTP:', error);
+    } finally {
+      client.close();
+    }
 
     res.status(200).json({
       success: true,
       clientInfo: {
         consultations,
-        surveys
-        
+        surveys,
+        documents
       }
     });
   } catch (error) {
