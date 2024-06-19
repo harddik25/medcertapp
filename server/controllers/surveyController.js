ba22df1e895591a8d3e5012097529d51198caaa3
harddik25 <135815117+harddik25@users.noreply.github.com>
2024-06-19 16:42:38 +0200

    Update surveyController.js

--- server/controllers/surveyController.js
+++ server/controllers/surveyController.js
@@ -2,11 +2,12 @@ const Survey = require('../models/Survey');
 
 exports.saveSurvey = async (req, res) => {
   try {
-    const { telegramId, ...surveyData } = req.body;
-    const survey = new Survey({ telegramId, ...surveyData });
+    const surveyData = req.body;
+    const survey = new Survey(surveyData);
     await survey.save();
     res.status(201).json({ message: 'Survey saved successfully' });
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    console.error('Error saving survey data:', error);
+    res.status(500).json({ error: 'Internal Server Error', details: error.message });
   }
 };
