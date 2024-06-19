8ad88cb4f5086524770721dd8fb2397ae3d9818e
harddik25 <135815117+harddik25@users.noreply.github.com>
2024-06-19 23:02:20 +0200

    Update surveyRoutes.js

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { saveSurvey,surveyController } = require('../controllers/surveyController');
+const { saveSurvey } = require('../controllers/surveyController');
 
 router.post('/save', saveSurvey);
-router.get('/latest/:userId', surveyController.getLatestSurveyId);
+
 
 module.exports = router;
