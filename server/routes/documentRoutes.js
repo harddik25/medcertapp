5dc20ccf6ccfdba64678f68fef8a159509fc3095
harddik25 <135815117+harddik25@users.noreply.github.com>
2024-06-19 22:52:37 +0200

    Update documentRoutes.js

--- server/routes/documentRoutes.js
+++ server/routes/documentRoutes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const documentController = require('../controllers/documentController');
 const upload = require('./upload'); // Импортируем middleware для загрузки файлов
 
-router.post('/upload', upload.single('document'), documentController.uploadDocument);
+router.post('/upload', upload.fields([{ name: 'frontDocument', maxCount: 1 }, { name: 'backDocument', maxCount: 1 }]), documentController.uploadDocument);
 
 module.exports = router;
