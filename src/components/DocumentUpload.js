65d7fcf0f5c3e8d9ee01029ce10a1eefbccc93ec
harddik25 <135815117+harddik25@users.noreply.github.com>
2024-06-20 13:31:47 +0200

    Update DocumentUpload.js

--- src/components/DocumentUpload.js
+++ src/components/DocumentUpload.js
@@ -90,7 +90,7 @@ const DocumentUpload = () => {
       setUploadStatus('Документ успешно загружен');
       setOpenSnackbar(true);
       setTimeout(() => {
-        navigate('/survey');
+        navigate('/main-survey');
       }, 2000);
     } catch (error) {
       setUploadStatus('Ошибка при загрузке документа');
