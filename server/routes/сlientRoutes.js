55a001bc06121d190395912619da21bc10c8698d
harddik25 <135815117+harddik25@users.noreply.github.com>
2024-06-21 16:33:32 +0200

    Update сlientRoutes.js

--- server/routes/сlientRoutes.js
+++ server/routes/сlientRoutes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const clientController = require('../controllers/clientController');
+const upload = require('../middlewares/multer');
 
+router.post('/documents/upload-certificate', upload.single('certificate'), clientController.uploadCertificate);
 router.get('/info/:patientId', clientController.getClientInfo);
 
 module.exports = router;
