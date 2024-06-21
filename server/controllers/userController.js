c4ace0fd3d0e005a97c06f7a1e4ef0e1a67813b6
harddik25 <135815117+harddik25@users.noreply.github.com>
2024-06-21 17:43:48 +0200

    Update userController.js

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,7 +1,7 @@
 const User = require('../models/User');
 const fs = require('fs');
 const path = require('path');
-
+const ftp = require('basic-ftp');
 exports.getUserRoleByTelegramId = async (req, res) => {
   try {
     const { telegramId } = req.params;
@@ -37,28 +37,39 @@ exports.getUserById = async (req, res) => {
 exports.getCertificate = async (req, res) => {
   try {
     const { userId } = req.params;
-    const certificatePath = path.join(`/var/www/user4806313/data/${userId}/certificate`);
+    const certificatePath = `/var/www/user4806313/data/${userId}/certificate`;
 
     console.log('Fetching certificate for user:', userId); // Логирование ID пользователя
     console.log('Certificate path:', certificatePath);
 
-    if (!fs.existsSync(certificatePath)) {
-      console.log('Certificate path does not exist:', certificatePath); // Логирование отсутствия пути
-      return res.status(404).json({ message: 'Certificate not found' });
-    }
+    const client = new ftp.Client();
+    client.ftp.verbose = true;
 
-    const files = fs.readdirSync(certificatePath);
-    if (files.length === 0) {
-      console.log('No certificate files found in path:', certificatePath); // Логирование отсутствия файлов
-      return res.status(404).json({ message: 'Certificate not found' });
-    }
+    try {
+      await client.access({
+        host: process.env.FTP_HOST,
+        user: process.env.FTP_USER,
+        password: process.env.FTP_PASSWORD,
+        secure: false,
+      });
 
-    const filename = files[0]; // Assuming there's only one certificate per user
-    console.log('Found certificate:', filename); // Логирование найденного файла
-    res.status(200).json({ filename });
+      const list = await client.list(certificatePath);
+      if (list.length === 0) {
+        console.log('No certificate files found in path:', certificatePath); // Логирование отсутствия файлов
+        return res.status(404).json({ message: 'Certificate not found' });
+      }
+
+      const filename = list[0].name; // Assuming there's only one certificate per user
+      console.log('Found certificate:', filename); // Логирование найденного файла
+      res.status(200).json({ filename });
+    } catch (error) {
+      console.error('Error accessing FTP server:', error);
+      res.status(500).json({ message: 'Server error', error: error.message });
+    } finally {
+      client.close();
+    }
   } catch (error) {
     console.error('Error fetching certificate:', error);
     res.status(500).json({ message: 'Server error' });
   }
 };
-
