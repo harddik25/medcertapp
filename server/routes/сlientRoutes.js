29ee464ecf67f52a795defe69d603ec437334132
harddik25 <135815117+harddik25@users.noreply.github.com>
2024-06-21 16:48:01 +0200

    Update сlientRoutes.js

--- server/routes/сlientRoutes.js
+++ server/routes/сlientRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const clientController = require('../controllers/clientController');
 const upload = require('./upload');
 
-router.post('/documents/upload-certificate', upload.single('certificate'), clientController.uploadCertificate);
+router.post('/upload-certificate', upload.single('certificate'), clientController.uploadCertificate);
 router.get('/info/:patientId', clientController.getClientInfo);
 
 module.exports = router;
