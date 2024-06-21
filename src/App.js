9d9d948fd805df01fa937413d1b8d12861f69c69
harddik25 <135815117+harddik25@users.noreply.github.com>
2024-06-21 14:41:18 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
 import UserProfile from './components/UserProfile';
 import Certificate from './components/Certificate';
@@ -15,7 +15,6 @@ import DocumentUpload from './components/DocumentUpload';
 import MainSurvey from './components/MainSurvey';
 import ProtectedRoute from './components/ProtectedRoute';
 import ClientInfo from './components/ClientInfo';
-import i18n from './i18n';
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -35,33 +34,33 @@ function App() {
   return (
     <>
       <WebSocketComponent />
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/language" element={<LanguageSelection />} />
-        <Route path="/profile" element={<UserProfile />} />
-        <Route path="/certificate" element={<Certificate />} />
-        <Route path="/consultation" element={<Consultation />} />
-        <Route path="/certificate-info" element={<CertificateInfo />} />
-        <Route path="/agreement" element={<Agreement />} />
-        <Route path="/document-upload" element={<DocumentUpload />} />
-        <Route path="/main-survey" element={<MainSurvey />} />
-        <Route path="/" element={<Login />} />
-      
-        <Route path="/doctor" element={
-          <ProtectedRoute allowedRoles={['doctor', 'admin']}>
-            <DoctorPanel />
-          </ProtectedRoute>
-        } />
-        <Route path="/doctor/client-info/:patientId" element={<ClientInfo />} />
-        <Route path="/admin" element={
-          <ProtectedRoute allowedRoles={['admin']}>
-            <AdminPanel />
-          </ProtectedRoute>
-        } />
-      </Routes>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/language" element={<LanguageSelection />} />
+          <Route path="/profile" element={<UserProfile />} />
+          <Route path="/certificate" element={<Certificate />} />
+          <Route path="/consultation" element={<Consultation />} />
+          <Route path="/certificate-info" element={<CertificateInfo />} />
+          <Route path="/agreement" element={<Agreement />} />
+          <Route path="/document-upload" element={<DocumentUpload />} />
+          <Route path="/main-survey" element={<MainSurvey />} />
+          <Route path="/" element={<Login />} />
+          <Route path="/doctor" element={
+            <ProtectedRoute allowedRoles={['doctor', 'admin']}>
+              <DoctorPanel />
+            </ProtectedRoute>
+          } />
+          <Route path="/doctor/client-info/:patientId" element={<ClientInfo />} />
+          <Route path="/admin" element={
+            <ProtectedRoute allowedRoles={['admin']}>
+              <AdminPanel />
+            </ProtectedRoute>
+          } />
+        </Routes>
+      </Router>
     </>
   );
 }
 
 export default App;
-
