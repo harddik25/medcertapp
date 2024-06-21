bbaf0c20a15ab36fa95e51e0bd3fa3f547307a9a
harddik25 <135815117+harddik25@users.noreply.github.com>
2024-06-21 14:04:30 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,8 +4,11 @@ import './index.css';
 import App from './App';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { BrowserRouter } from 'react-router-dom';
-
+import reportWebVitals from './reportWebVitals';
+import { I18nextProvider } from 'react-i18next';
+import i18n from './i18n';
 const theme = createTheme({
+
   palette: {
     primary: {
       main: '#1976d2',
@@ -16,13 +19,15 @@ const theme = createTheme({
   },
 });
 
+
 ReactDOM.render(
-  <ThemeProvider theme={theme}>
-    <BrowserRouter>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    </BrowserRouter>
-  </ThemeProvider>,
+  <React.StrictMode>
+    <I18nextProvider i18n={i18n}>
+      <App />
+    </I18nextProvider>
+  </React.StrictMode>,
   document.getElementById('root')
 );
+
+reportWebVitals();
+
