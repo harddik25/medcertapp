ff870c977fd64fbebcdcc2ab7073ddd7a736a271
harddik25 <135815117+harddik25@users.noreply.github.com>
2024-06-21 20:30:41 +0200

    Update consultationRoutes.js

--- server/routes/consultationRoutes.js
+++ server/routes/consultationRoutes.js
@@ -8,5 +8,6 @@ router.post('/book', consultationController.bookFreeSlot);
 router.get('/appointments/:patientName', consultationController.getAppointments);
 router.get('/future-appointments', consultationController.getFutureAppointments);
 router.delete('/free-slots/:slotId', consultationController.deleteFreeSlot);
+router.delete('/appointments/:patientId', consultationController.deleteAppointmentByPatientId);
 
 module.exports = router;
