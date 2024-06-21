2b266adfa5300f4105c6c1ecb85662c80fb6c155
harddik25 <135815117+harddik25@users.noreply.github.com>
2024-06-21 14:03:56 +0200

    Create i18n.js

---
+++ src/i18n.js
@@ -0,0 +1,66 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import HttpApi from 'i18next-http-backend';
+
+// Переводы
+const resources = {
+  en: {
+    translation: {
+      "Welcome": "Welcome",
+      "Video Consultation": "Video Consultation",
+      "Date": "Date",
+      "Time": "Time",
+      "Book": "Book",
+      "Sorry, no available slots at the moment": "Sorry, no available slots at the moment",
+      "Success! Your appointment is booked at": "Success! Your appointment is booked at",
+      "Back": "Back",
+      "Client Information": "Client Information",
+      "Survey Answers": "Survey Answers",
+      "Question": "Question",
+      "Answer": "Answer",
+      "Download Front Side": "Download Front Side",
+      "Download Back Side": "Download Back Side",
+      "Name": "Name",
+      "Pathologies": "Pathologies",
+      "No": "No"
+      // Add other translations as needed
+    }
+  },
+  es: {
+    translation: {
+      "Welcome": "Bienvenido",
+      "Video Consultation": "Consulta de video",
+      "Date": "Fecha",
+      "Time": "Hora",
+      "Book": "Reservar",
+      "Sorry, no available slots at the moment": "Lo sentimos, no hay citas disponibles en este momento",
+      "Success! Your appointment is booked at": "¡Éxito! Tu cita está reservada a las",
+      "Back": "Atrás",
+      "Client Information": "Información del cliente",
+      "Survey Answers": "Respuestas de la encuesta",
+      "Question": "Pregunta",
+      "Answer": "Respuesta",
+      "Download Front Side": "Descargar lado frontal",
+      "Download Back Side": "Descargar lado posterior",
+      "Name": "Nombre",
+      "Pathologies": "Patologías",
+      "No": "No"
+      // Add other translations as needed
+    }
+  }
+};
+
+i18n
+  .use(initReactI18next)
+  .use(LanguageDetector)
+  .use(HttpApi)
+  .init({
+    resources,
+    fallbackLng: 'en',
+    interpolation: {
+      escapeValue: false
+    }
+  });
+
+export default i18n;
