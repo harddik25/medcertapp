3243b836a03ec069fd33a12567ceeafd261e889a
harddik25 <135815117+harddik25@users.noreply.github.com>
2024-06-21 16:42:45 +0200

    Update app.js

--- server/app.js
+++ server/app.js
@@ -21,8 +21,8 @@ const app = express();
 
 
 app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 app.use(cookieParser());
 
 const uri = process.env.MONGO_URI;
