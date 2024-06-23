82e4e6661819bf1ad58cd75f38aba74c6f103ff8
harddik25 <135815117+harddik25@users.noreply.github.com>
2024-06-23 15:44:35 +0200

    Update UserProfile.js

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,19 +1,44 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { Container, Box, Typography, Button, CssBaseline, Avatar, Paper, Snackbar, Alert } from '@mui/material';
-import { deepOrange } from '@mui/material/colors';
-import { styled } from '@mui/system';
+import { green, deepOrange } from '@mui/material/colors';
+import { styled, createTheme, ThemeProvider } from '@mui/material/styles';
 import CannabisBackground from './cannabis-background.webp';
 import { useTranslation } from 'react-i18next';
 import PersonIcon from '@mui/icons-material/Person';
 import LanguageSwitcher from './LanguageSwitcher';
+
+const theme = createTheme({
+  typography: {
+    fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
+    h5: {
+      fontWeight: 600,
+      letterSpacing: '0.5px',
+    },
+    body1: {
+      fontSize: '1rem',
+      letterSpacing: '0.5px',
+    },
+  },
+  palette: {
+    primary: {
+      main: green[500],
+    },
+    secondary: {
+      main: deepOrange[500],
+    },
+    background: {
+      default: '#f5f5f5',
+    },
+  },
+});
 
 const Background = styled('div')({
   display: 'flex',
   alignItems: 'center',
   justifyContent: 'center',
   height: '100vh',
-  backgroundImage: url(${CannabisBackground}),
+  backgroundImage: `url(${CannabisBackground})`,
   backgroundSize: 'cover',
 });
 
@@ -23,6 +48,28 @@ const Header = styled(Box)({
   justifyContent: 'space-between',
   width: '100%',
   marginBottom: 16,
+});
+
+const StyledPaper = styled(Paper)({
+  padding: 24,
+  backgroundColor: 'rgba(255, 255, 255, 0.9)',
+  borderRadius: 16,
+  boxShadow: '0 8px 16px rgba(0,0,0,0.2)',
+});
+
+const StyledButton = styled(Button)({
+  marginBottom: 16,
+  padding: '12px 24px',
+  textTransform: 'none',
+  borderRadius: 8,
+  fontSize: '1.2rem',
+  color: '#000',
+  backgroundColor: '#96f296',
+  boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.1)',
+  '&:hover': {
+    backgroundColor: '#82e082',
+    boxShadow: '0px 6px 8px rgba(0, 0, 0, 0.2)',
+  },
 });
 
 const UserProfile = () => {
@@ -39,7 +86,7 @@ const UserProfile = () => {
       const telegramUser = JSON.parse(localStorage.getItem('telegramUser'));
       if (telegramUser) {
         try {
-          const response = await fetch(https://medlevel.me/api/users/role/${telegramUser.id});
+          const response = await fetch(`https://medlevel.me/api/users/role/${telegramUser.id}`);
           const data = await response.json();
           const userWithRole = { ...telegramUser, role: data.role };
           setUser(userWithRole);
@@ -58,7 +105,7 @@ const UserProfile = () => {
       if (user) {
         try {
           console.log('Fetching certificate for user:', user.id); // Логирование ID пользователя
-          const response = await fetch(https://medlevel.me/api/users/certificate/${user.id});
+          const response = await fetch(`https://medlevel.me/api/users/certificate/${user.id}`);
           const data = await response.json();
           console.log('Received certificate data:', data); // Логирование полученных данных сертификата
           if (data.filename) {
@@ -80,7 +127,7 @@ const UserProfile = () => {
       if (user) {
         try {
           console.log('Fetching appointment for user:', user.id);
-          const response = await fetch(https://medlevel.me/api/consultations/appointments/${user.id});
+          const response = await fetch(`https://medlevel.me/api/consultations/appointments/${user.id}`);
           const responseText = await response.text(); // Получаем текстовый ответ для логирования
           console.log('Response text:', responseText); // Логируем текстовый ответ
           const data = JSON.parse(responseText); // Парсим JSON вручную для отладки
@@ -117,7 +164,7 @@ const UserProfile = () => {
 
   const handleJoinConsultation = () => {
     const currentTime = new Date();
-    const appointmentDate = new Date(${appointment.date}T${appointment.time}:00);
+    const appointmentDate = new Date(`${appointment.date}T${appointment.time}:00`);
 
     const timeDifference = (appointmentDate - currentTime) / 1000 / 60; // Разница во времени в минутах
 
@@ -134,123 +181,117 @@ const UserProfile = () => {
   };
 
   return (
-    <Background>
-      <Container component="main" maxWidth="md">
-        <CssBaseline />
-        <Paper elevation={3} sx={{ padding: 3, backgroundColor: 'rgba(255, 255, 255, 0.8)' }}>
-          <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-            <Header>
+    <ThemeProvider theme={theme}>
+      <Background>
+        <Container component="main" maxWidth="md">
+          <CssBaseline />
+          <StyledPaper elevation={3}>
+            <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+              <Header>
+                {user && (
+                  <>
+                    <Avatar sx={{ bgcolor: deepOrange[500], width: 80, height: 80 }}>
+                      <PersonIcon sx={{ fontSize: 40 }} />
+                    </Avatar>
+                    <LanguageSwitcher />
+                  </>
+                )}
+              </Header>
               {user && (
                 <>
-                  <Avatar sx={{ bgcolor: deepOrange[500], width: 80, height: 80 }}>
-                    <PersonIcon sx={{ fontSize: 40 }} />
-                  </Avatar>
-                  <LanguageSwitcher />
-                </>
-              )}
-            </Header>
-            {user && (
-              <>
-                <Typography component="h1" variant="h5" sx={{ color: '#388e3c' }}>
-                  {t('User Profile')}
-                </Typography>
-                <Typography variant="body1" sx={{ mt: 2, mb: 4, color: '#4caf50' }}>
-                  {t('Welcome')}, {user.first_name}!
-                </Typography>
-                {user.role === 'admin' && (
-                  <>
-                    <Button
+                  <Typography component="h1" variant="h5" sx={{ color: theme.palette.primary.main }}>
+                    {t('User Profile')}
+                  </Typography>
+                  <Typography variant="body1" sx={{ mt: 2, mb: 4, color: theme.palette.primary.main }}>
+                    {t('Welcome')}, {user.first_name}!
+                  </Typography>
+                  {user.role === 'admin' && (
+                    <>
+                      <StyledButton
+                        fullWidth
+                        variant="contained"
+                        onClick={handleAdminPanel}
+                      >
+                        {t('Admin Panel')}
+                      </StyledButton>
+                      <StyledButton
+                        fullWidth
+                        variant="contained"
+                        onClick={handleDoctorPanel}
+                      >
+                        {t('Doctor Panel')}
+                      </StyledButton>
+                    </>
+                  )}
+                  {user.role === 'doctor' && (
+                    <StyledButton
                       fullWidth
                       variant="contained"
-                      sx={{ mb: 2, backgroundColor: '#f44336', color: '#fff' }}
-                      onClick={handleAdminPanel}
-                    >
-                      {t('Admin Panel')}
-                    </Button>
-                    <Button
-                      fullWidth
-                      variant="contained"
-                      sx={{ mb: 2, backgroundColor: '#1976d2', color: '#fff' }}
                       onClick={handleDoctorPanel}
                     >
                       {t('Doctor Panel')}
-                    </Button>
-                  </>
-                )}
-                {user.role === 'doctor' && (
-                  <Button
-                    fullWidth
-                    variant="contained"
-                    sx={{ mb: 2, backgroundColor: '#1976d2', color: '#fff' }}
-                    onClick={handleDoctorPanel}
-                  >
-                    {t('Doctor Panel')}
-                  </Button>
-                )}
-                {certificate ? (
-                  <Button
-                    fullWidth
-                    variant="contained"
-                    sx={{ mb: 2, backgroundColor: '#4caf50', color: '#fff' }}
-                    href={https://medlevel.me/api/documents/download/certificate/${user.id}/${certificate}}
-                    target="_blank"
-                  >
-                    {t('Download Certificate')}
-                  </Button>
-                ) : (
-                  <>
-                    {appointment ? (
-                      <>
-                        <Typography variant="body1" sx={{ mt: 2, mb: 4, color: '#4caf50' }}>
-                          {t('Your consultation appointment is on')} {appointment.date} {appointment.time}
-                        </Typography>
-                        {appointment.telegramLink && (
-                          <Button
-                            fullWidth
-                            variant="contained"
-                            sx={{ mb: 2, backgroundColor: '#4caf50', color: '#fff' }}
-                            onClick={handleJoinConsultation}
-                          >
-                            {t('Join Consultation')}
-                          </Button>
-                        )}
-                      </>
-                    ) : (
-                      <Button
-                        fullWidth
-                        variant="contained"
-                        sx={{ mb: 2, backgroundColor: '#4caf50', color: '#fff' }}
-                        onClick={handleCertificateInfo}
-                      >
-                        {t('About Certificate')}
-                      </Button>
-                    )}
-                  </>
-                )}
-                {!certificate && !appointment && (
-                  <Button
-                    fullWidth
-                    variant="contained"
-                    sx={{ mb: 2, backgroundColor: '#4caf50', color: '#fff' }}
-                    onClick={handleBuyCertificate}
-                  >
-                    {t('Buy Certificate')}
-                  </Button>
-                )}
-              </>
-            )}
-          </Box>
-        </Paper>
-        <Snackbar open={openSnackbar} autoHideDuration={6000} onClose={handleCloseSnackbar}>
-          <Alert onClose={handleCloseSnackbar} severity="warning">
-            {snackbarMessage}
-          </Alert>
-        </Snackbar>
-      </Container>
-    </Background>
+                    </StyledButton>
+                  )}
+                  {certificate ? (
+                    <StyledButton
+                      fullWidth
+                      variant="contained"
+                      href={`https://medlevel.me/api/documents/download/certificate/${user.id}/${certificate}`}
+                      target="_blank"
+                    >
+                      {t('Download Certificate')}
+                    </StyledButton>
+                  ) : (
+                    <>
+                      {appointment ? (
+                        <>
+                          <Typography variant="body1" sx={{ mt: 2, mb: 4, color: theme.palette.primary.main }}>
+                            {t('Your consultation appointment is on')} {appointment.date} {appointment.time}
+                          </Typography>
+                          {appointment.telegramLink && (
+                            <StyledButton
+                              fullWidth
+                              variant="contained"
+                              onClick={handleJoinConsultation}
+                            >
+                              {t('Join Consultation')}
+                            </StyledButton>
+                          )}
+                        </>
+                      ) : (
+                        <StyledButton
+                          fullWidth
+                          variant="contained"
+                          onClick={handleCertificateInfo}
+                        >
+                          {t('About Certificate')}
+                        </StyledButton>
+                      )}
+                    </>
+                  )}
+                  {!certificate && !appointment && (
+                    <StyledButton
+                      fullWidth
+                      variant="contained"
+                      onClick={handleBuyCertificate}
+                    >
+                      {t('Buy Certificate')}
+                    </StyledButton>
+                  )}
+                </>
+              )}
+            </Box>
+          </StyledPaper>
+          <Snackbar open={openSnackbar} autoHideDuration={6000} onClose={handleCloseSnackbar}>
+            <Alert onClose={handleCloseSnackbar} severity="warning">
+              {snackbarMessage}
+            </Alert>
+          </Snackbar>
+        </Container>
+      </Background>
+    </ThemeProvider>
   );
 };
 
 export default UserProfile;
-
 
